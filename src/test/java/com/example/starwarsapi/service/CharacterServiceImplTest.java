22a8e188bb6b66cf8728ea91e03e6b2fda8d6491
Mykyta Shmatchenko <mykyta.shmatchenko@signanthealth.com>
2024-03-14 10:00:04 +0100

    update test

--- src/test/java/com/example/starwarsapi/service/CharacterServiceImplTest.java
+++ src/test/java/com/example/starwarsapi/service/CharacterServiceImplTest.java
@@ -58,25 +58,37 @@ public class CharacterServiceImplTest {
 
     @Test
     void getHeaviestCharacterOnPlanet() {
-        String planetName = "Tatooine";
-        Planet planet = new Planet();
-        planet.setName(planetName);
+        String planetName1 = "Tatooine";
+        Planet planet1 = new Planet();
+        planet1.setName(planetName1);
+        String planetName2 = "Naboo";
+        Planet planet2 = new Planet();
+        planet2.setName(planetName2);
+
         Character char1 = new Character();
-        char1.setPlanet(planet);
+        char1.setPlanet(planet1);
         char1.setName("Luke Skywalker");
         char1.setMass(77);
         Character char2 = new Character();
-        char2.setPlanet(planet);
+        char2.setPlanet(planet1);
         char2.setName("Leia Organa");
         char2.setMass(49);
         Character char3 = new Character();
-        char3.setPlanet(planet);
+        char3.setPlanet(planet1);
         char3.setName("C-3PO");
         char3.setMass(75);
+        Character char4 = new Character();
+        char4.setPlanet(planet2);
+        char4.setName("R2-D2");
+        char4.setMass(75);
+        Character char5 = new Character();
+        char5.setPlanet(planet2);
+        char5.setName("Darth Vader");
+        char5.setMass(136);
 
-        when(characterRepository.getAllCharacters()).thenReturn(List.of(char1, char2, char3));
+        when(characterRepository.getAllCharacters()).thenReturn(List.of(char1, char2, char3, char4, char5));
 
-        Character result = characterService.getHeaviestCharacterOnPlanet(planetName);
+        Character result = characterService.getHeaviestCharacterOnPlanet(planetName1);
 
         verify(characterRepository).getAllCharacters();
         assertThat(result).isEqualTo(char1);
